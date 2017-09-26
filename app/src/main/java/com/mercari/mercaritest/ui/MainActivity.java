93a6196b3a7bdd4cb64c434053091ab683a379f9
bschiranth <bschiranth@gmail.com>
2017-09-25 20:20:38 -0700

    recycler view set up with adapter

--- app/src/main/java/com/mercari/mercaritest/ui/MainActivity.java
+++ app/src/main/java/com/mercari/mercaritest/ui/MainActivity.java
@@ -2,14 +2,35 @@ package com.mercari.mercaritest.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.mercari.mercaritest.R;
+import com.mercari.mercaritest.utils.Constants;
+import com.mercari.mercaritest.utils.JsonUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerViewId)
+    RecyclerView recyclerView;
+
+    SaleAdapter saleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, Constants.NUMBER_OF_COLUMNS));
+        saleAdapter = new SaleAdapter(this);
+        recyclerView.setAdapter(saleAdapter);
+        recyclerView.setHasFixedSize(true);
+
+        JsonUtils.parseJson(this,saleAdapter);
     }
 }
